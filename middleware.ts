4da644a1da01cd6892e01896c20022be27e9d04d
taehoonkim <10bdhoon@gmail.com>
2026-05-18 10:55:19 +0900

    make Kim Myunghui onboarding guide public

--- middleware.ts
+++ middleware.ts
@@ -50,6 +50,8 @@ export default async function middleware(req: Request) {
     url.pathname === "/onboarding-guide.html" ||
     url.pathname === "/onboarding-guide-v2" ||
     url.pathname === "/onboarding-guide-v2.html" ||
+    url.pathname === "/onboarding-guide-kim-myunghui" ||
+    url.pathname === "/onboarding-guide-kim-myunghui.html" ||
     url.pathname === "/jungmin-request" ||
     url.pathname === "/jungmin-request.html" ||
     url.pathname === "/customer-experience-map" ||
